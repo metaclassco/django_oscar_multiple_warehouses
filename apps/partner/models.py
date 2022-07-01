f1a002d66948b4c8ce5820f9bc9841e31188961a
Alexander Gaevsky <sasha@metaclass.co>
2022-07-01 12:32:27 +0300

    Outline warehouse model.

--- apps/partner/models.py
+++ apps/partner/models.py
@@ -1,1 +1,13 @@
+from django.db import models
+
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=255)
+    partner = models.ForeignKey('partner.Partner', related_name='warehouses', on_delete=models.CASCADE)
+    address = models.OneToOneField('address.WarehouseAddress', on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.name
+
+
 from oscar.apps.partner.models import *  # noqa isort:skip
