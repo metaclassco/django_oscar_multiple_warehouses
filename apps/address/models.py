138547444bd4e20bdbc08ad65c097fc084b54120
Alexander Gaevsky <sasha@metaclass.co>
2022-07-01 12:30:47 +0300

    Outline warehouse address model.

--- apps/address/models.py
+++ apps/address/models.py
@@ -1,1 +1,8 @@
+from oscar.apps.address.abstract_models import AbstractAddress
+
+
+class WarehouseAddress(AbstractAddress):
+    pass
+
+
 from oscar.apps.address.models import *  # noqa isort:skip
